1075ecc4230d201060b5bf55ca76d47bef2e4716
khori moore <khorimoore23@gmail.com>
2024-10-03 13:45:38 -0700

    staged server.ts page

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -6,9 +6,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
-// Define route to serve index.html
+
 router.get('/', (_req, res) => {
-    const filePath = path.join(__dirname, '../client/index.html');  // Adjust the path as needed
+    const filePath = path.join(__dirname, '../client/index.html');  
     res.sendFile(filePath);
 }); 
 
