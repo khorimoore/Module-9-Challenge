0de9e19930c34532b4a8d65ca827f99ce8deafad
khori moore <khorimoore23@gmail.com>
2024-10-03 13:47:05 -0700

    staged htmlroutes.ts page

--- Develop/server/src/routes/htmlRoutes.ts
+++ Develop/server/src/routes/htmlRoutes.ts
@@ -6,9 +6,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
-// TODO: Define route to serve index.html
+
 router.get('/', (_req, res) => {
-    const filePath = path.join(__dirname, '../client/index.html');  // Adjust the path as per your project structure
+    const filePath = path.join(__dirname, '../client/index.html'); 
     res.sendFile(filePath);
 });
 
