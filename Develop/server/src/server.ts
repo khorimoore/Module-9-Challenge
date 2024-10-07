56a03d1c6e5d5dd6c05459d9b6e1a1b1fc06a1fa
khori moore <khorimoore23@gmail.com>
2024-10-07 09:36:02 -0700

    updated server.ts

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -1,16 +1,23 @@
-import express, { Router } from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
 
 const app = express();
-const router = Router();
 const PORT = process.env.PORT || 3000;
 
-router.get('/', (_req, res) => {
-    const filePath = path.join(__dirname, '../client/index.html');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('../client/dist'));
+
+app.get('/', (_req, res) => {
+    const filePath = path.join(__dirname, '../../client/index.html');
     res.sendFile(filePath);
 });
 
-app.use(router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
